99d4d4a32950bba6c8532290d7898e67db76eaef
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-19 23:57:44 +0300

    Create Console.cs

---
+++ Console.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string word;
+            word = "Yeditepe University";
+            Console.WriteLine(word);
+            int number = 5;
+            Console.Write(number);
+            Console.Read();
+        }
+    }
+}
