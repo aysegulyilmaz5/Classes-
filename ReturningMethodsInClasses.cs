44204bbe02c4e6dc08f2a251ef1acebcae838a2d
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-20 02:14:46 +0300

    Create ReturningMethodsInClasses.cs

---
+++ ReturningMethodsInClasses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations op = new Operations();
+            op.sum(7, 6);
+            op.sum(4, 5);
+            op.square(5);
+            op.square(8);
+            Console.Read();
+
+        }
+    }
+}
