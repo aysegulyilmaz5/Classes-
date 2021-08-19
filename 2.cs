86db6c31dd628fdca2702ab493781141809381d1
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-20 02:02:26 +0300

    Create 2.cs

---
+++ 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            string name1;
+            Console.Write("Enter name:");
+            name1 = Console.ReadLine();
+            user.User_name(name1);
+            string surname1;
+            Console.Write("Enter a surname:");
+            surname1 = Console.ReadLine();
+            user.User_surname(surname1);
+            string job1;
+            Console.Write("Enter a job:");
+            job1 = Console.ReadLine();
+            user.User_job(job1);
+            Console.Read();
+        }
+    }
+}
