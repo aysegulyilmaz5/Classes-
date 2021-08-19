3b13b19fefa30658a7a68b7bb73a0801662c3154
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-20 02:01:56 +0300

    Create 1.cs

---
+++ 1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NotReturningMethodsInClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Users us = new Users();
+            string ads;
+            Console.Write("Enter a name:");
+            ads = Console.ReadLine();
+            us.User_List(ads);
+            Console.Read();
+        }
+    }
+}
